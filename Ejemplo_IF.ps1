1cc7719fcc2729c58b11d24798f787fb2caef17c
Rodrigo Torres Rivera <rodrigo.torres4234@alumnos.udg.mx>
2025-04-11 04:17:02 -0600

    Agregando scripts de ejemplo

---
+++ Ejemplo_IF.ps1
@@ -0,0 +1,19 @@
+Write-Host "Opción verdadera"
+Write-Host ""
+$respuesta="True"
+if ($respuesta -eq "true"){
+	Write-Host "Hola Mundo"
+}
+else{
+	Write-Host "Adios mundo"
+}
+Write-Host ""
+Write-Host "Opción Falsa"
+Write-Host ""
+$respuesta="false"
+if($respuesta -eq "true"){
+	Write-Host "Fola Mundo"
+}
+else{
+	Write-Host "Odios Mundo"
+}
